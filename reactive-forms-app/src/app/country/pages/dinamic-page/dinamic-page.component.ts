8066c4cb78200665bab64791976e58d333a0ab2e
Xavier Cobos <xavier.cobos@securitydata.net.ec>
2026-04-01 07:06:08 -0500

    feat: controles en FormArray y finalización form dinámico

--- reactive-forms-app/src/app/country/pages/dinamic-page/dinamic-page.component.ts
+++ reactive-forms-app/src/app/country/pages/dinamic-page/dinamic-page.component.ts
@@ -1,12 +1,59 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { FormUtils } from '../../../utils/form.util';
 
 @Component({
   selector: 'app-dinamic-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './dinamic-page.component.html',
-  styleUrl: './dinamic-page.component.css'
+  styleUrl: './dinamic-page.component.css',
 })
 export class DinamicPageComponent {
+  private fb = inject(FormBuilder);
 
+  formUtils = FormUtils; //calase de utilidades para mensajes de errro de los fomrularios
+
+  newFavorite = new FormControl('', Validators.required); //crear un control es mejor de esta forma por legibilidad
+  // newFavorite = this.fb.control([])
+
+  myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    favoriteGames: this.fb.array(
+      [
+        ['Metal Gear', Validators.required],
+        ['God Of war', Validators.required],
+      ],
+      Validators.minLength(2),
+    ),
+  });
+
+  get favoriteGames() {
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+
+  onAddToFavorites() {
+    if (this.newFavorite.invalid) return;
+    console.log('pasa');
+    const newGame = this.newFavorite.value;
+
+    this.favoriteGames.push(this.fb.control(newGame, Validators.required));
+
+    this.newFavorite.reset();
+  }
+
+  onDeleteFavorites(index: number) {
+    this.favoriteGames.removeAt(index);
+  }
+
+  onSave() {
+    this.myForm.markAllAsTouched();
+  }
 }
--- reactive-forms-app/src/app/utils/form.util.ts
+++ reactive-forms-app/src/app/utils/form.util.ts
@@ -1,35 +1,62 @@
-import { FormGroup } from '@angular/forms';
+import { FormArray, FormGroup } from '@angular/forms';
 
 export class FormUtils {
-    static isValidField(form:FormGroup, fieldName: string): boolean | null {
-
+  static isValidField(form: FormGroup, fieldName: string): boolean | null {
     return form.controls[fieldName].errors && form.controls[fieldName].touched;
   }
 
-  static getFieldError(form:FormGroup, fieldName:string): string | null{
-    if(!form.controls[fieldName]) return null;
+  static getFieldError(form: FormGroup, fieldName: string): string | null {
+    if (!form.controls[fieldName]) return null;
 
-    const errors = form.controls[fieldName].errors ?? {}
+    const errors = form.controls[fieldName].errors ?? {};
 
-    console.log("Errores", errors);
+    for (const key of Object.keys(errors)) {
+      console.log('key', key);
 
-     for(const key of Object.keys(errors)){
-      console.log("key", key);
-
-      switch(key){
+      switch (key) {
         case 'required':
           return 'Este campo es requerido';
 
         case 'minlength':
-          return `Mínimo de ${errors['minlength'].requiredLength} caracteres`
+          return `Mínimo de ${errors['minlength'].requiredLength} caracteres`;
 
-          case 'min':
-          return `Valor mínimo de ${errors['min'].min}`
-
+        case 'min':
+          return `Valor mínimo de ${errors['min'].min}`;
       }
-     }
+    }
 
-     return null
+    return null;
   }
 
+  static isValidFieldInArray(formArray: FormArray, index: number) {
+    return (
+      formArray.controls[index].errors && formArray.controls[index].touched
+    );
+  }
+
+  static getFieldArrayError(
+    formArray: FormArray,
+    index: number,
+  ): string | null {
+    if (!formArray.controls[index]) return null;
+
+    const errors = formArray.controls[index].errors ?? {};
+
+    for (const key of Object.keys(errors)) {
+      console.log('key', key);
+
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido';
+
+        case 'minlength':
+          return `Mínimo de ${errors['minlength'].requiredLength} caracteres`;
+
+        case 'min':
+          return `Valor mínimo de ${errors['min'].min}`;
+      }
+    }
+
+    return null;
+  }
 }
